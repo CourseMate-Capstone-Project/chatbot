62cc69b2c1c0b3e1c8116e5238eafceda560e9dd
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2024-12-08 02:18:33 +0700

    chatbot vertex

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def generate_message():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
